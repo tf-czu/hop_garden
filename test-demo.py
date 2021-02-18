f7ff97d1f94f44d130a8d13d9ddc20712da8848b
tajgr <levjakub@gmail.com>
2021-02-18 18:06:42 +0100

    test-demo.py: test for sort_plants function

--- test-demo.py
+++ test-demo.py
@@ -1,5 +1,5 @@
 import unittest
-from demo import fill_polyline
+from demo import fill_polyline, sort_plants
 import numpy as np
 
 
@@ -12,6 +12,13 @@ class MyTestCase(unittest.TestCase):
         polyline = np.asarray([[1, 4, 6, 8], [5, 4, 4, 6]])
         expected_ret = np.asarray([[1, 2, 3, 4, 5, 6, 7, 8], [5, 5, 4, 4, 4, 4, 5, 6]])
         self.assertTrue(np.array_equal(fill_polyline(polyline), expected_ret))
+
+    def test_sort_plants(self):
+        row = np.array([[0, 1],[1,1]])
+        centroids = np.array([[5,4], [1,2], [4,4]])
+        expected_ret = np.array([[1, 2], [4, 4], [5, 4]])
+        ret, dist = sort_plants(row, centroids)
+        self.assertTrue(np.array_equal(ret, expected_ret))
 
 
 if __name__ == '__main__':
